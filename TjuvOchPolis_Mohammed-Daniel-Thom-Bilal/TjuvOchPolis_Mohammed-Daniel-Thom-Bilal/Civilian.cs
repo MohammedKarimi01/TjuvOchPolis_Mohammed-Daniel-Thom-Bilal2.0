0644ee4b71cbc9ad359ba2a86e6e4694f3c6698c
Mohammed Karimi <mohammed.karimi01@hotmail.com>
2022-10-17 14:22:46 +0200

    Lagt till klasser
    Lagt till klasser

---
+++ TjuvOchPolis_Mohammed-Daniel-Thom-Bilal/TjuvOchPolis_Mohammed-Daniel-Thom-Bilal/Civilian.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TjuvOchPolis_Mohammed_Daniel_Thom_Bilal
+{
+    internal class Civilian
+    {
+
+    }
+}
---
+++ TjuvOchPolis_Mohammed-Daniel-Thom-Bilal/TjuvOchPolis_Mohammed-Daniel-Thom-Bilal/Thief.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TjuvOchPolis_Mohammed_Daniel_Thom_Bilal
+{
+    internal class Thief
+    {
+    }
+}
