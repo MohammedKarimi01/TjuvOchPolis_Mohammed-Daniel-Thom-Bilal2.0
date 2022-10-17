1185597ca57a6b6afcfec2f1e0ec9236aa0b9a28
thomf20 <thom.flodqvist@gmail.com>
2022-10-17 15:05:21 +0000

    Added properties to class Persons

---
+++ TjuvOchPolis_Mohammed-Daniel-Thom-Bilal/TjuvOchPolis_Mohammed-Daniel-Thom-Bilal/Persons.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TjuvOchPolis_Mohammed_Daniel_Thom_Bilal
+{
+    internal class Persons
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int Xdirection { get; set; }
+        public int Ydirection { get; set; }
+        public string inventory { get; set; }
+
+        public Persons()
+        {
+
+        }
+
+    }
+}
--- TjuvOchPolis_Mohammed-Daniel-Thom-Bilal/TjuvOchPolis_Mohammed-Daniel-Thom-Bilal/Program.cs
+++ TjuvOchPolis_Mohammed-Daniel-Thom-Bilal/TjuvOchPolis_Mohammed-Daniel-Thom-Bilal/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.writeline("Testing//Mohammed");
+Console.WriteLine("Testing//Mohammed");
 Console.WriteLine("Hola");
