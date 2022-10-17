601cd39d03527ed337eca71b223f072a08ce2f21
Mohammed Karimi <mohammed.karimi01@hotmail.com>
2022-10-17 15:19:30 +0200

    Civilians
    Lagt till lite saker i Civilians

--- TjuvOchPolis_Mohammed-Daniel-Thom-Bilal/TjuvOchPolis_Mohammed-Daniel-Thom-Bilal/Civilian.cs
+++ TjuvOchPolis_Mohammed-Daniel-Thom-Bilal/TjuvOchPolis_Mohammed-Daniel-Thom-Bilal/Civilian.cs
@@ -6,8 +6,27 @@ using System.Threading.Tasks;
 
 namespace TjuvOchPolis_Mohammed_Daniel_Thom_Bilal
 {
-    internal class Civilian
+    internal class Civilian : Persons
     {
+        public class Inventory : Civilian
+        {
+            public class Position : Civilian
+            {
+                Random random = new Random();
+                string[,] position = new string[25, 101];
 
+                int XAxel = random.next(0,26);
+                int YAxel = random.next(0,101);
+                string Civilian = "M";
+            }
+            public list<String> Belongings = new list<String>();
+            public inventory()
+            {
+                Belongings.Add("Klocka");
+                Belongings.add("Plånbok");
+                Belongings.add("Pengar");
+                Belongings.add("Mobil");
+            }
+        }
     }
 }
